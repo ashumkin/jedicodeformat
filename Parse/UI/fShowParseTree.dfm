4af66f1b35adf000cfb78f904ed1ff6e47f96963
anthonysteele <anthonysteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2002-12-30 15:39:19 +0000

    show token list as well as parse tree
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@26 375ba82b-2d0e-0410-9568-ec9673457f6e

--- Parse/UI/fShowParseTree.dfm
+++ Parse/UI/fShowParseTree.dfm
@@ -11,21 +11,9 @@ object frmShowParseTree: TfrmShowParseTree
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
-  object tvParseTree: TTreeView
-    Left = 0
-    Top = 69
-    Width = 459
-    Height = 258
-    Align = alClient
-    HideSelection = False
-    Indent = 15
-    MultiSelectStyle = []
-    ReadOnly = True
-    TabOrder = 0
-    OnChange = tvParseTreeChange
-  end
   object StatusBar1: TStatusBar
     Left = 0
     Top = 368
@@ -40,7 +28,7 @@ object frmShowParseTree: TfrmShowParseTree
     Height = 69
     Align = alTop
     BevelOuter = bvNone
-    TabOrder = 2
+    TabOrder = 1
     object lblTreeCount: TLabel
       Left = 8
       Top = 24
@@ -72,7 +60,7 @@ object frmShowParseTree: TfrmShowParseTree
     Height = 41
     Align = alBottom
     BevelOuter = bvNone
-    TabOrder = 3
+    TabOrder = 2
     object lblCurrent: TLabel
       Left = 8
       Top = 6
@@ -100,6 +88,59 @@ object frmShowParseTree: TfrmShowParseTree
       Width = 106
       Height = 13
       Caption = 'Immediate child count:'
+    end
+  end
+  object pcPages: TPageControl
+    Left = 0
+    Top = 69
+    Width = 459
+    Height = 258
+    ActivePage = tsTokens
+    Align = alClient
+    TabOrder = 3
+    object tsTokens: TTabSheet
+      Caption = 'Tokens'
+      object lvTokens: TListView
+        Left = 0
+        Top = 0
+        Width = 451
+        Height = 230
+        Align = alClient
+        Columns = <
+          item
+            Caption = 'Index'
+          end
+          item
+            Caption = 'Type'
+            Width = 150
+          end
+          item
+            Caption = 'Text'
+            Width = 220
+          end>
+        ReadOnly = True
+        RowSelect = True
+        TabOrder = 0
+        ViewStyle = vsReport
+        OnSelectItem = lvTokensSelectItem
+      end
+    end
+    object tsTree: TTabSheet
+      Caption = 'Tree'
+      ImageIndex = 1
+      object tvParseTree: TTreeView
+        Left = 0
+        Top = 0
+        Width = 451
+        Height = 230
+        Align = alClient
+        HideSelection = False
+        Indent = 15
+        MultiSelectStyle = []
+        ReadOnly = True
+        TabOrder = 0
+        OnChange = tvParseTreeChange
+      end
     end
   end
 end
--- Parse/UI/fShowParseTree.pas
+++ Parse/UI/fShowParseTree.pas
@@ -1,5 +1,13 @@
 unit fShowParseTree;
 
+{
+  AFS 2002
+
+  A form to show a unit's parse tree
+  mainly for debugiing purposes when the parse goes wrong
+}
+
+
 interface
 
 uses
@@ -11,7 +19,6 @@ uses
 
 type
   TfrmShowParseTree = class(TForm)
-    tvParseTree: TTreeView;
     StatusBar1: TStatusBar;
     pnlTop: TPanel;
     lblTreeCount: TLabel;
@@ -22,14 +29,24 @@ type
     lblTotalNodeCount: TLabel;
     lblImmediateChildCount: TLabel;
     cbShowWhiteSpace: TCheckBox;
+    pcPages: TPageControl;
+    tsTokens: TTabSheet;
+    tsTree: TTabSheet;
+    tvParseTree: TTreeView;
+    lvTokens: TListView;
     procedure tvParseTreeChange(Sender: TObject; Node: TTreeNode);
     procedure cbShowWhiteSpaceClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure lvTokensClick(Sender: TObject);
+    procedure lvTokensSelectItem(Sender: TObject; Item: TListItem;
+      Selected: Boolean);
   private
     fcRootNode: TParseTreeNode;
+    procedure ShowTreeNodeDetails(const pcNode: TParseTreeNode);
   public
     property RootNode: TParseTreeNode read fcRootNode write fcRootNode;
 
-    procedure MakeTree;
+    procedure DisplayTree;
   end;
 
 procedure ShowParseTree(const pcRoot: TParseTreeNode);
@@ -38,7 +55,7 @@ implementation
 
 {$R *.dfm}
 
-uses SourceToken, TokenType;
+uses SourceToken, TokenType, WordMap;
 
 procedure ShowParseTree(const pcRoot: TParseTreeNode);
 var
@@ -47,7 +64,7 @@ begin
   lfParseTree := TfrmShowParseTree.Create(Application);
   try
     lfParseTree.RootNode := pcRoot;
-    lfParseTree.MakeTree;
+    lfParseTree.DisplayTree;
 
     lfParseTree.ShowModal;
 
@@ -58,7 +75,41 @@ end;
 
 
 
-procedure TfrmShowParseTree.MakeTree;
+procedure TfrmShowParseTree.DisplayTree;
+
+  procedure ShowTokensInList(const pcData: TParseTreeNode);
+  var
+    lcNewItem: TListItem;
+    lcToken: TSourceToken;
+    liLoop: integer;
+    lsDesc: string;
+  begin
+    { exclude this one as white space }
+    if (not cbShowWhiteSpace.Checked) and (not pcData.HasChildren) and
+      (pcData is TSourceToken) and (TSourceToken(pcData).TokenType in NotSolidTokens) then
+        exit;
+
+    { list tokens }
+    if (pcData is TSourceToken) then
+    begin
+      lcToken := TSourceToken(pcData);
+
+      lcNewItem := lvTokens.Items.Add;
+      lcNewItem.Caption := IntToStr(lvTokens.Items.Count);
+
+      lsDesc := TokenTypeToString(lcToken.TokenType);
+      if lcToken.Word <> wUnknown then
+        lsDesc := lsDesc + ' ' + WordToString(lcToken.Word);
+      lcNewItem.SubItems.Add(lsDesc);
+      lcNewItem.SubItems.Add(lcToken.SourceCode);
+
+       lcNewItem.Data := pcData;
+    end;
+
+    // attach the children
+    for liLoop := 0 to  pcData.ChildNodeCount - 1 do
+      ShowTokensInList(pcData.ChildNodes[liLoop]);
+  end;
 
   procedure MakeNodeChildren(const pcGUIParent: TTreeNode; const pcData: TParseTreeNode);
   var
@@ -79,25 +130,44 @@ procedure TfrmShowParseTree.MakeTree;
   end;
 
 begin
+  lblTreeCount.Caption := 'Tree has ' + IntToStr(fcRootNode.RecursiveChildCount) + ' nodes';
+  lblTreeDepth.Caption := 'Tree has max depth of ' + IntToStr(fcRootNode.MaxDepth);
+
+
+  lvTokens.Items.BeginUpdate;
+  try
+    lvTokens.Items.Clear;
+
+    ShowTokensInList(fcRootNode);
+
+  finally
+    lvTokens.Items.EndUpdate;
+  end;
+
   tvParseTree.Items.BeginUpdate;
   try
     tvParseTree.Items.Clear;
     MakeNodeChildren(nil, fcRootNode);
 
-    lblTreeCount.Caption := 'Tree has ' + IntToStr(fcRootNode.RecursiveChildCount) + ' nodes';
-    lblTreeDepth.Caption := 'Tree has max depth of ' + IntToStr(fcRootNode.MaxDepth);
-
     tvParseTree.FullExpand;
   finally
     tvParseTree.Items.EndUpdate;
   end;
+
 end;
 
 procedure TfrmShowParseTree.tvParseTreeChange(Sender: TObject;  Node: TTreeNode);
-var
-  lcParseTreeNode: TParseTreeNode;
 begin
   if Node = nil then
+    ShowTreeNodeDetails(nil)
+  else
+    ShowTreeNodeDetails(Node.Data);
+end;
+
+procedure TfrmShowParseTree.ShowTreeNodeDetails(const pcNode: TParseTreeNode);
+begin
+
+  if pcNode = nil then
   begin
     lblCurrent.Caption := 'Current: none';
     lblDepth.Caption := 'Depth: -';
@@ -106,19 +176,39 @@ begin
   end
   else
   begin
-    lcParseTreeNode := Node.Data;
-
-    lblCurrent.Caption := 'Current: ' + lcParseTreeNode.Describe;
-    lblDepth.Caption := 'Level: ' + IntToStr(lcParseTreeNode.Level);
-    lblImmediateChildCount.Caption := 'Immediate child count: ' + IntToStr(lcParseTreeNode.ChildNodeCount);
-    lblTotalNodeCount.Caption := 'Total node count: ' + IntToStr(lcParseTreeNode.RecursiveChildCount);
+    lblCurrent.Caption := 'Current: ' + pcNode.Describe;
+    lblDepth.Caption := 'Level: ' + IntToStr(pcNode.Level);
+    lblImmediateChildCount.Caption := 'Immediate child count: ' + IntToStr(pcNode.ChildNodeCount);
+    lblTotalNodeCount.Caption := 'Total node count: ' + IntToStr(pcNode.RecursiveChildCount);
   end;
 end;
 
 procedure TfrmShowParseTree.cbShowWhiteSpaceClick(Sender: TObject);
 begin
   // ShowWhiteSpace setting has changed. Redisplay
-  MakeTree;
+  DisplayTree;
+end;
+
+procedure TfrmShowParseTree.FormShow(Sender: TObject);
+begin
+  pcPages.ActivePage := tsTree;
+end;
+
+procedure TfrmShowParseTree.lvTokensClick(Sender: TObject);
+begin
+  if lvTokens.Selected = nil then
+    ShowTreeNodeDetails(nil)
+  else
+    ShowTreeNodeDetails(lvTokens.Selected.Data);
+end;
+
+procedure TfrmShowParseTree.lvTokensSelectItem(Sender: TObject;
+  Item: TListItem; Selected: Boolean);
+begin
+  if lvTokens.Selected = nil then
+    ShowTreeNodeDetails(nil)
+  else
+    ShowTreeNodeDetails(lvTokens.Selected.Data);
 end;
 
 end.
