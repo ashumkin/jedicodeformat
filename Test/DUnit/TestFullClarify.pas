7da0cbddbc0afb9ca3aedd4477ef8d9ced5647e7
anthonysteele <anthonysteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2003-03-05 23:37:06 +0000

    working on test cases for the indenter
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@59 375ba82b-2d0e-0410-9568-ec9673457f6e

--- Test/DUnit/TestFullClarify.pas
+++ Test/DUnit/TestFullClarify.pas
@@ -39,6 +39,57 @@ type
   procedure TestClarify_TestClassLines;
   procedure TestClarify_TestCommentIndent;
   procedure TestClarify_TestConstRecords;
+  procedure TestClarify_TestD6;
+  procedure TestClarify_TestDeclarations2;
+  procedure TestClarify_TestDeclarations;
+  procedure TestClarify_TestDefaultParams;
+  procedure TestClarify_TestEmptyClass;
+  procedure TestClarify_TestEsotericKeywords;
+  procedure TestClarify_TestExclusion;
+  procedure TestClarify_TestExclusionFlags;
+  procedure TestClarify_TestExternal;
+  procedure TestClarify_TestForward;
+  procedure TestClarify_TestGoto;
+  procedure TestClarify_TestInitFinal;
+  procedure TestClarify_TestInterfaceImplements;
+  procedure TestClarify_TestInterfaceMap;
+  procedure TestClarify_TestInterfaces;
+  procedure TestClarify_TestLayout;
+  procedure TestClarify_TestLayoutBare2;
+  procedure TestClarify_TestLayoutBare3;
+  procedure TestClarify_TestLayoutBare;
+  procedure TestClarify_TestLibExports;
+
+  procedure TestClarify_TestLineBreaking;
+  procedure TestClarify_TestLocalTypes;
+  procedure TestClarify_TestLongStrings;
+  procedure TestClarify_TestMarcoV;
+  procedure TestClarify_TestMH;
+  procedure TestClarify_TestMixedModeCaps;
+  procedure TestClarify_TestMVB;
+  procedure TestClarify_TestNested;
+  procedure TestClarify_TestNestedRecords;
+  procedure TestClarify_TestOperators;
+  procedure TestClarify_TestParams;
+  procedure TestClarify_TestParamSpaces;
+  procedure TestClarify_TestPointers;
+  procedure TestClarify_TestProgram;
+  procedure TestClarify_TestProperties;
+  procedure TestClarify_TestPropertyLines;
+  procedure TestClarify_TestRecords;
+  procedure TestClarify_TestReg;
+  procedure TestClarify_TestReint;
+  procedure TestClarify_TestReturnRemoval;
+  procedure TestClarify_TestReturns;
+  procedure TestClarify_TestRunOnConst;
+  procedure TestClarify_TestRunOnDef;
+  procedure TestClarify_TestRunOnLine;
+  procedure TestClarify_TestTPObjects;
+  procedure TestClarify_TestTry;
+  procedure TestClarify_TestTypeDefs;
+  procedure TestClarify_TestUses;
+  procedure TestClarify_TestUsesChanges;
+  procedure TestClarify_TestWarnings;
 end;
 
 
@@ -236,6 +287,256 @@ end;
 procedure TTestClarify.TestClarify_TestConstRecords;
 begin
   TestClarifyFile('TestConstRecords');
+end;
+
+procedure TTestClarify.TestClarify_TestD6;
+begin
+  TestClarifyFile('TestD6');
+end;
+
+procedure TTestClarify.TestClarify_TestDeclarations;
+begin
+  TestClarifyFile('TestDeclarations');
+end;
+
+procedure TTestClarify.TestClarify_TestDeclarations2;
+begin
+  TestClarifyFile('TestDeclarations2');
+end;
+
+procedure TTestClarify.TestClarify_TestDefaultParams;
+begin
+  TestClarifyFile('TestDefaultParams');
+end;
+
+procedure TTestClarify.TestClarify_TestEmptyClass;
+begin
+  TestClarifyFile('TestEmptyClass');
+end;
+
+procedure TTestClarify.TestClarify_TestEsotericKeywords;
+begin
+  TestClarifyFile('TestEsotericKeywords');
+end;
+
+procedure TTestClarify.TestClarify_TestExclusion;
+begin
+  TestClarifyFile('TestExclusion');
+end;
+
+procedure TTestClarify.TestClarify_TestExclusionFlags;
+begin
+  TestClarifyFile('TestExclusionFlags');
+end;
+
+procedure TTestClarify.TestClarify_TestExternal;
+begin
+  TestClarifyFile('TestExternal');
+end;
+
+procedure TTestClarify.TestClarify_TestForward;
+begin
+  TestClarifyFile('TestForward');
+end;
+
+procedure TTestClarify.TestClarify_TestGoto;
+begin
+  TestClarifyFile('TestGoto');
+end;
+
+procedure TTestClarify.TestClarify_TestInitFinal;
+begin
+  TestClarifyFile('TestInitFinal');
+end;
+
+procedure TTestClarify.TestClarify_TestInterfaceImplements;
+begin
+  TestClarifyFile('TestInterfaceImplements');
+end;
+
+procedure TTestClarify.TestClarify_TestInterfaceMap;
+begin
+  TestClarifyFile('TestInterfaceMap');
+end;
+
+procedure TTestClarify.TestClarify_TestInterfaces;
+begin
+  TestClarifyFile('TestInterfaces');
+end;
+
+procedure TTestClarify.TestClarify_TestLayout;
+begin
+  TestClarifyFile('TestLayout');
+end;
+
+procedure TTestClarify.TestClarify_TestLayoutBare;
+begin
+  TestClarifyFile('TestLayoutBare');
+end;
+
+procedure TTestClarify.TestClarify_TestLayoutBare2;
+begin
+  TestClarifyFile('TestLayoutBare2');
+end;
+
+procedure TTestClarify.TestClarify_TestLayoutBare3;
+begin
+  TestClarifyFile('TestLayoutBare3');
+end;
+
+procedure TTestClarify.TestClarify_TestLibExports;
+begin
+  TestClarifyFile('TestLibExports');
+end;
+
+procedure TTestClarify.TestClarify_TestLineBreaking;
+begin
+  TestClarifyFile('TestLineBreaking');
+end;
+
+procedure TTestClarify.TestClarify_TestLocalTypes;
+begin
+  TestClarifyFile('TestLocalTypes');
+end;
+
+procedure TTestClarify.TestClarify_TestLongStrings;
+begin
+  TestClarifyFile('TestLongStrings');
+end;
+
+procedure TTestClarify.TestClarify_TestMarcoV;
+begin
+  TestClarifyFile('TestMarcoV');
+end;
+
+procedure TTestClarify.TestClarify_TestMH;
+begin
+  TestClarifyFile('TestMH');
+end;
+
+procedure TTestClarify.TestClarify_TestMixedModeCaps;
+begin
+  TestClarifyFile('TestMixedModeCaps');
+end;
+
+procedure TTestClarify.TestClarify_TestMVB;
+begin
+  TestClarifyFile('TestMVB');
+end;
+
+procedure TTestClarify.TestClarify_TestNested;
+begin
+  TestClarifyFile('TestNested');
+end;
+
+procedure TTestClarify.TestClarify_TestNestedRecords;
+begin
+  TestClarifyFile('TestNestedRecords');
+end;
+
+procedure TTestClarify.TestClarify_TestOperators;
+begin
+  TestClarifyFile('TestOperators');
+end;
+
+procedure TTestClarify.TestClarify_TestParams;
+begin
+  TestClarifyFile('TestParams');
+end;
+
+procedure TTestClarify.TestClarify_TestParamSpaces;
+begin
+  TestClarifyFile('TestParamSpaces');
+end;
+
+procedure TTestClarify.TestClarify_TestPointers;
+begin
+  TestClarifyFile('TestPointers');
+end;
+
+procedure TTestClarify.TestClarify_TestProgram;
+begin
+  TestClarifyFile('TestProgram');
+end;
+
+procedure TTestClarify.TestClarify_TestProperties;
+begin
+  TestClarifyFile('TestProperties');
+end;
+
+procedure TTestClarify.TestClarify_TestPropertyLines;
+begin
+  TestClarifyFile('TestPropertyLines');
+end;
+
+procedure TTestClarify.TestClarify_TestRecords;
+begin
+  TestClarifyFile('TestRecords');
+end;
+
+procedure TTestClarify.TestClarify_TestReg;
+begin
+  TestClarifyFile('TestReg');
+end;
+
+procedure TTestClarify.TestClarify_TestReint;
+begin
+  TestClarifyFile('TestReint');
+end;
+
+procedure TTestClarify.TestClarify_TestReturnRemoval;
+begin
+  TestClarifyFile('TestReturnRemoval');
+end;
+
+procedure TTestClarify.TestClarify_TestReturns;
+begin
+  TestClarifyFile('TestReturns');
+end;
+
+procedure TTestClarify.TestClarify_TestRunOnConst;
+begin
+  TestClarifyFile('TestRunOnConst');
+end;
+
+procedure TTestClarify.TestClarify_TestRunOnDef;
+begin
+  TestClarifyFile('TestRunOnDef');
+end;
+
+procedure TTestClarify.TestClarify_TestRunOnLine;
+begin
+  TestClarifyFile('TestRunOnLine');
+end;
+
+procedure TTestClarify.TestClarify_TestTPObjects;
+begin
+  TestClarifyFile('TestTPObjects');
+end;
+
+procedure TTestClarify.TestClarify_TestTry;
+begin
+  TestClarifyFile('TestTry');
+end;
+
+procedure TTestClarify.TestClarify_TestTypeDefs;
+begin
+  TestClarifyFile('TestTypeDefs');
+end;
+
+procedure TTestClarify.TestClarify_TestUses;
+begin
+  TestClarifyFile('TestUses');
+end;
+
+procedure TTestClarify.TestClarify_TestUsesChanges;
+begin
+  TestClarifyFile('TestUsesChanges');
+end;
+
+procedure TTestClarify.TestClarify_TestWarnings;
+begin
+  TestClarifyFile('TestWarnings');
 end;
 
 initialization
