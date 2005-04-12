96b64fb89254b191a8120e0c22d351d0e7f3fb63
anthonysteele <anthonysteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2005-04-12 21:41:42 +0000

    *** empty log message ***
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@377 375ba82b-2d0e-0410-9568-ec9673457f6e

---
+++ Test/DUnit/TestFormatPart.pas
@@ -0,0 +1,77 @@
+unit TestFormatPart;
+
+interface
+
+uses
+  TestFrameWork,
+  BaseTestProcess, SettingsTypes;
+
+type
+  TTestFormatPart = class(TBaseTestProcess)
+  private
+
+  protected
+    procedure Setup; override;
+    procedure Teardown; override;
+  published
+    procedure Test1;
+    procedure Test2;
+  end;
+
+implementation
+
+uses JclStrings, BlockStyles, JcfSettings, SetReturns;
+
+{ TTestFormatPart }
+
+const
+  UNIT_IN = UNIT_HEADER +
+    'procedure fred; ' + AnsiLineBreak +
+    'begin' + AnsiLineBreak +
+    'end;'  + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_OUT_TOP = SPACED_UNIT_HEADER +
+    'procedure fred; ' + AnsiLineBreak +
+    'begin' + AnsiLineBreak +
+    'end;'  + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_OUT_TOP_MID = SPACED_UNIT_HEADER +  AnsiLineBreak +
+    'procedure fred; ' + AnsiLineBreak +
+    'begin' + AnsiLineBreak +
+    'end;'  + AnsiLineBreak +
+    UNIT_FOOTER;
+
+
+  UNIT_OUT_MID = UNIT_HEADER + 
+    'procedure fred; ' + AnsiLineBreak +
+    'begin' + AnsiLineBreak +
+    'end;'  + AnsiLineBreak +
+    UNIT_FOOTER;
+
+procedure TTestFormatPart.Setup;
+begin
+  inherited;
+
+end;
+
+procedure TTestFormatPart.Teardown;
+begin
+  inherited;
+
+end;
+
+procedure TTestFormatPart.Test1;
+begin
+  TestFormatPartResult(UNIT_IN, UNIT_OUT_TOP, 10, 20);
+end;
+
+procedure TTestFormatPart.Test2;
+begin
+  TestFormatPartResult(UNIT_IN, UNIT_OUT_TOP_MID, 20, 30);
+end;
+
+initialization
+  TestFramework.RegisterTest('Processes', TTestFormatPart.Suite);
+end.
