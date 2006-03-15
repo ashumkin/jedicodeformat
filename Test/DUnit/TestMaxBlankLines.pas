89eeb951ca4306883f841495833e2eae03dd853a
AnthonySteele <AnthonySteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2006-03-15 21:05:06 +0000

    Test cases for SF bug 1440877: Remove blank lines after procedure header bug
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@439 375ba82b-2d0e-0410-9568-ec9673457f6e

--- Test/DUnit/TestMaxBlankLines.pas
+++ Test/DUnit/TestMaxBlankLines.pas
@@ -33,24 +33,55 @@ uses
 type
   TTestRemoveBlankLines = class(TBaseTestProcess)
   private
+    fiMaxLines, fiMaxLinesInSection: integer;
+    fbRemoveCosecutiveBlankLines: Boolean;
   public
     procedure SetUp; override;
+    procedure TearDown; override;
   published
     procedure TestNone;
     procedure Test1;
+
+    procedure TestVarLines0;
+    procedure TestVarLines1;
+    procedure TestVarLines2;
+    procedure TestVarLines3;
+    procedure TestVarLines4;
+
+    procedure TestProcHeaderLines0;
+    procedure TestProcHeaderLines1;
+    procedure TestProcHeaderLines2;
+    procedure TestProcHeaderLines3;
+    procedure TestProcHeaderLines4;
   end;
 
 implementation
 
 uses
   JclStrings,
-  JcfSettings, RemoveConsecutiveReturns;
+  JcfSettings, SetReturns,
+  RemoveConsecutiveReturns, RemoveBlankLinesInVars, RemoveBlankLinesAfterProcHeader;
 
 procedure TTestRemoveBlankLines.Setup;
 begin
   inherited;
+  fiMaxLines := FormatSettings.Returns.MaxConsecutiveBlankLines;
+  fiMaxLinesInSection := FormatSettings.Returns.MaxBlankLinesInSection;
+  fbRemoveCosecutiveBlankLines := FormatSettings.Returns.RemoveConsecutiveBlankLines;
+
   FormatSettings.Returns.MaxConsecutiveBlankLines    := 4;
   FormatSettings.Returns.RemoveConsecutiveBlankLines := True;
+end;
+
+
+procedure TTestRemoveBlankLines.TearDown;
+begin
+  inherited;
+
+  // restore initial values
+  FormatSettings.Returns.MaxConsecutiveBlankLines := fiMaxLines;
+  FormatSettings.Returns.MaxBlankLinesInSection := fiMaxLinesInSection;
+  FormatSettings.Returns.RemoveConsecutiveBlankLines := fbRemoveCosecutiveBlankLines;
 end;
 
 procedure TTestRemoveBlankLines.TestNone;
@@ -75,6 +106,169 @@ const
     UNIT_FOOTER;
 begin
   TestProcessResult(TRemoveConsecutiveReturns, IN_UNIT_TEXT, OUT_UNIT_TEXT);
+end;
+
+
+const
+  UNIT_TEXT_VAR_LINES_0  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_TEXT_VAR_LINES_1  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_TEXT_VAR_LINES_2  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    AnsiLineBreak +
+    AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+
+  UNIT_TEXT_VAR_LINES_3  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    AnsiLineBreak +
+    AnsiLineBreak +
+    AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+
+procedure TTestRemoveBlankLines.TestVarLines0;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 0;
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_0, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_1, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_2, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_3, UNIT_TEXT_VAR_LINES_0);
+end;
+
+procedure TTestRemoveBlankLines.TestVarLines1;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 1;
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_0, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_1, UNIT_TEXT_VAR_LINES_1);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_2, UNIT_TEXT_VAR_LINES_1);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_3, UNIT_TEXT_VAR_LINES_1);
+end;
+
+procedure TTestRemoveBlankLines.TestVarLines2;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 2;
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_0, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_1, UNIT_TEXT_VAR_LINES_1);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_2, UNIT_TEXT_VAR_LINES_2);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_3, UNIT_TEXT_VAR_LINES_2);
+end;
+
+procedure TTestRemoveBlankLines.TestVarLines3;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 3;
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_0, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_1, UNIT_TEXT_VAR_LINES_1);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_2, UNIT_TEXT_VAR_LINES_2);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_3, UNIT_TEXT_VAR_LINES_3);
+end;
+
+procedure TTestRemoveBlankLines.TestVarLines4;
+begin
+  // as per 3
+  FormatSettings.Returns.MaxBlankLinesInSection := 4;
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_0, UNIT_TEXT_VAR_LINES_0);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_1, UNIT_TEXT_VAR_LINES_1);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_2, UNIT_TEXT_VAR_LINES_2);
+  TestProcessResult(TRemoveBlankLinesInVars, UNIT_TEXT_VAR_LINES_3, UNIT_TEXT_VAR_LINES_3);
+end;
+
+const
+  UNIT_TEXT_HEADER_LINES_0  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_TEXT_HEADER_LINES_1  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+     AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_TEXT_HEADER_LINES_2  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+     AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+  UNIT_TEXT_HEADER_LINES_3  = UNIT_HEADER +
+    ' procedure foo; ' + AnsiLineBreak +
+     AnsiLineBreak +
+    'var' + AnsiLineBreak +
+    '  a: integer;' + AnsiLineBreak +
+    'begin end;' + AnsiLineBreak +
+    UNIT_FOOTER;
+
+
+
+procedure TTestRemoveBlankLines.TestProcHeaderLines0;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 0;
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_0, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_1, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_2, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_3, UNIT_TEXT_HEADER_LINES_0);
+end;
+
+procedure TTestRemoveBlankLines.TestProcHeaderLines1;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 1;
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_0, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_1, UNIT_TEXT_HEADER_LINES_1);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_2, UNIT_TEXT_HEADER_LINES_1);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_3, UNIT_TEXT_HEADER_LINES_1);
+end;
+
+procedure TTestRemoveBlankLines.TestProcHeaderLines2;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 2;
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_0, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_1, UNIT_TEXT_HEADER_LINES_1);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_2, UNIT_TEXT_HEADER_LINES_2);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_3, UNIT_TEXT_HEADER_LINES_2);
+end;
+
+procedure TTestRemoveBlankLines.TestProcHeaderLines3;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 3;
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_0, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_1, UNIT_TEXT_HEADER_LINES_1);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_2, UNIT_TEXT_HEADER_LINES_2);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_3, UNIT_TEXT_HEADER_LINES_3);
+end;
+
+procedure TTestRemoveBlankLines.TestProcHeaderLines4;
+begin
+  FormatSettings.Returns.MaxBlankLinesInSection := 4;
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_0, UNIT_TEXT_HEADER_LINES_0);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_1, UNIT_TEXT_HEADER_LINES_1);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_2, UNIT_TEXT_HEADER_LINES_2);
+  TestProcessResult(TRemoveBlankLinesAfterProcHeader, UNIT_TEXT_HEADER_LINES_3, UNIT_TEXT_HEADER_LINES_3);
 end;
 
 initialization
