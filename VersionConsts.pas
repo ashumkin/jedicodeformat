bde81c4cdb6aab7d7b7e0430d5d6791a808544d7
anthonysteele <anthonysteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2007-07-21 19:51:45 +0000

    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@512 375ba82b-2d0e-0410-9568-ec9673457f6e

--- VersionConsts.pas
+++ VersionConsts.pas
@@ -25,7 +25,7 @@ interface
 
 const
   PROGRAM_VERSION   = '2.30';
-  PROGRAM_DATE      = 'June 2007';
+  PROGRAM_DATE      = 'July 2007';
   PROGRAM_HOME_PAGE = 'http://jedicodeformat.sourceforge.net/';
   PROGRAM_SOURCEFORGE_HOME_PAGE = 'http://sourceforge.net/projects/jedicodeformat/';
   PROGRAM_SVN_TRUNK = 'https://svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2/';
