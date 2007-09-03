05792e33aad1323321c1006e7183a91fc067ae2c
anthonysteele <anthonysteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2007-09-03 20:31:38 +0000

    started on asm format options settings
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@530 375ba82b-2d0e-0410-9568-ec9673457f6e

---
+++ Settings/SetAsm.pas
@@ -0,0 +1,106 @@
+unit SetAsm;
+{(*}
+(*------------------------------------------------------------------------------
+ Delphi Code formatter source code 
+
+The Original Code is SetAsm.pas, released September 2007.
+The Initial Developer of the Original Code is Anthony Steele. 
+Portions created by Anthony Steele are Copyright (C) 2007 Anthony Steele.
+All Rights Reserved. 
+Contributor(s): Anthony Steele.
+
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"). you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.mozilla.org/NPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied.
+See the License for the specific language governing rights and limitations 
+under the License.
+------------------------------------------------------------------------------*)
+{*)}
+
+interface
+
+uses
+  JCFSetBase,
+  SettingsTypes, SettingsStream, IntList;
+
+type
+
+  TSetAsm = class(TSetBase)
+  private
+    feCapitalisation: TCapitalisationType;
+    fcIndents: TIntList;
+    function GetIndent(const index: integer): integer;
+    procedure SetIndent(const index: integer; const Value: integer);
+  protected
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure WriteToStream(const pcOut: TSettingsOutput); override;
+    procedure ReadFromStream(const pcStream: TSettingsInput); override;
+
+    property Capitalisation: TCapitalisationType Read feCapitalisation Write feCapitalisation;
+    property Indents[const index: integer]: integer read GetIndent write SetIndent;
+  end;
+
+implementation
+
+uses
+  SysUtils;
+
+const
+  LAST_INDENT_ITEM = 5;
+
+{ constuctor and destructor }
+
+constructor TSetAsm.Create;
+begin
+  inherited;
+  SetSection('Asm');
+
+   fcIndents := TIntList.Create;
+end;
+
+destructor TSetAsm.Destroy;
+begin
+  FreeAndNil(fcIndents);
+end;
+
+{ properties }
+
+function TSetAsm.GetIndent(const index: integer): integer;
+begin
+  // six values
+  if (index < 0) or (index > LAST_INDENT_ITEM) then
+  begin
+    Result := 0;
+  end
+  else
+  begin
+    Result := fcIndents.Items[index];
+  end;
+end;
+
+procedure TSetAsm.SetIndent(const index: integer; const Value: integer);
+begin
+  if (index >= 0) and (index <= LAST_INDENT_ITEM) then
+    fcIndents.Items[Index] := value;
+end;
+
+
+procedure TSetAsm.WriteToStream(const pcOut: TSettingsOutput);
+begin
+  inherited;
+
+end;
+
+procedure TSetAsm.ReadFromStream(const pcStream: TSettingsInput);
+begin
+  inherited;
+
+end;
+
+end.
