db52f8bd4aa6788761769e3db573ad072e901d1c
AnthonySteele <AnthonySteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2008-03-07 21:02:47 +0000

    work on unicode file reading
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@589 375ba82b-2d0e-0410-9568-ec9673457f6e

--- Utils/JcfUnicode.pas
+++ Utils/JcfUnicode.pas
@@ -25,7 +25,10 @@ interface
 
 type
   TFileContentType = (eUnknown, e8Bit, eUtf8,
-    eUtf16LittleEndian, eUtf16BigEndian);
+    eUtf16LittleEndian, eUtf16BigEndian,
+    eUtf32LittleEndian, eUtf32BigEndian);
+
+function TypeOfTextFile(const psFileName: string): TFileContentType;
 
 procedure ReadTextFile(const psFileName: string; out psContents: WideString;
   out peContentType: TFileContentType);
@@ -39,25 +42,7 @@ uses
   Classes, SysUtils;
 
 
-{ read in a text file,
-  the file can contain 8-bit or 16-bit chars
-  code adapted from a sample by Mike Shkolnik
-  in nntp://borland.public.delphi.rtl.general
-  Re: Read UNICODE/ANSI/ASCII Text File to WideString
-  at: Jan 23 2006, 12:17
-  found at http://delphi.newswhat.com/geoxml/forumhistorythread?groupname=borland.public.delphi.rtl.general&messageid=43d485bf$1@newsgroups.borland.com
-}
-procedure ReadTextFile(const psFileName: string; out psContents: WideString;
-  out peContentType: TFileContentType);
-var
-  fs: TFileStream;
-  wordRead: word;
-  byteRead: byte;
-  wideContents: WideString;
-  contents8bit: string;
-  liBytesRemaining: integer;
-  liLoop: integer;
-  lbResetPosition: boolean;
+function ReadFileHeader(const pcFileStream: TFileStream): TFileContentType;
 const
   // marker bytes at the start of the file
 
@@ -75,7 +60,148 @@ const
 
   Utf32BigEndianMarker1 = $0000;
   Utf32BigEndianMarker2 = $FFFE;
+var
+  word1: word;
+  word2: word;
+  byte3: byte;
+begin
+  // small files are ansi
+  if pcFileStream.Size < 4 then
+  begin
+    Result := e8Bit;
+    exit;
+  end;
 
+  // read the first 4 bytes
+  pcFileStream.Seek(0, soFromBeginning);
+
+  pcFileStream.Read(word1, SizeOf(word));
+  pcFileStream.Read(word2, SizeOf(word));
+
+  byte3 := byte(word2);
+
+  if  (word1 = Utf8Marker12) and (byte3 = Utf8Marker3) then
+  begin
+    Result := eUtf8;
+  end
+  else if (word1 = Utf16LittleEndianMarker) then
+  begin
+    Result := eUtf16LittleEndian;
+  end
+  else if (word1 = Utf16BigEndianMarker) then
+  begin
+      Result := eUtf16BigEndian;
+  end
+  else if (word1 = Utf32LittleEndianMarker1) and (word2 = Utf32LittleEndianMarker2) then
+  begin
+    Result := eUtf32LittleEndian;
+  end
+  else if (word1 = Utf32BigEndianMarker1) and (word2 = Utf32BigEndianMarker2) then
+  begin
+    Result := eUtf32BigEndian;
+  end
+  else
+  begin
+    Result := e8Bit;
+  end;
+
+end;
+
+function TypeOfTextFile(const psFileName: string): TFileContentType;
+var
+  fs: TFileStream;
+begin
+
+  {open file}
+  fs := TFileStream.Create(psFileName, fmOpenRead);
+  try
+    Result := ReadFileHeader(fs);
+  finally
+    // close the file
+    fs.Free;
+  end;
+end;
+
+procedure ReadPastFileHeader(const pcFileStream: TFileStream;
+  const peContentType: TFileContentType);
+var
+  liOffsetBytes: integer;
+begin
+
+  case peContentType of
+    e8Bit:
+      liOffsetBytes := 0;
+    eUtf8:
+      liOffsetBytes := 3;
+    eUtf16LittleEndian, eUtf16BigEndian:
+      liOffsetBytes := 2;
+    eUtf32LittleEndian, eUtf32BigEndian:
+      liOffsetBytes := 4;
+    else
+      raise Exception.Create('Unknown file content type: ' + IntToStr(Ord(peContentType)));
+
+  end;
+
+  pcFileStream.Seek(liOffsetBytes, soFromBeginning);
+end;
+
+function Read8BitFile(const pcFileStream: TFileStream): WideString;
+var
+  liBytesRemaining: integer;
+  lsContents8bit: string;
+begin
+  liBytesRemaining := pcFileStream.Size - pcFileStream.Position;
+  // read the bytes into a string
+  SetLength(lsContents8bit, liBytesRemaining);
+  if pcFileStream.Size > 0 then
+  begin
+    pcFileStream.ReadBuffer(lsContents8bit[1], liBytesRemaining);
+  end;
+
+  // convert to wide char
+  Result := lsContents8bit;
+end;
+
+function Read16BitFile(const pcFileStream: TFileStream; const pbBigEndian: boolean): WideString;
+var
+  liBytesRemaining: integer;
+  liLoop: integer;
+  lsWideContents: WideString;
+begin
+  // read it
+  liBytesRemaining := pcFileStream.Size - pcFileStream.Position;
+  SetLength(lsWideContents, liBytesRemaining div 2);
+  pcFileStream.Read(lsWideContents[1], liBytesRemaining);
+
+  if pbBigEndian then
+  begin
+    // swap the bytes
+    for liLoop := 1 to Length(lsWideContents) do
+      lsWideContents[liLoop] := widechar(Swap(word(lsWideContents[liLoop])));
+  end;
+
+  Result := lsWideContents;
+end;
+
+function Read32BitFile(const pcFileStream: TFileStream; pbBigEndian: boolean): WideString;
+begin
+  Result := '';
+end;
+
+
+
+{ read in a text file,
+  the file can contain 8-bit or 16-bit chars
+  code adapted from a sample by Mike Shkolnik
+  in nntp://borland.public.delphi.rtl.general
+  Re: Read UNICODE/ANSI/ASCII Text File to WideString
+  at: Jan 23 2006, 12:17
+  found at http://delphi.newswhat.com/geoxml/forumhistorythread?groupname=borland.public.delphi.rtl.general&messageid=43d485bf$1@newsgroups.borland.com
+}
+procedure ReadTextFile(const psFileName: string; out psContents: WideString;
+  out peContentType: TFileContentType);
+var
+  fs: TFileStream;
 begin
   psContents    := '';
   peContentType := eUnknown;
@@ -83,9 +209,33 @@ begin
   {open file}
   fs := TFileStream.Create(psFileName, fmOpenRead);
   try
+    peContentType := ReadFileHeader(fs);
 
-    { the stream can contain unicode characters -
-       we must check before parse }
+    ReadPastFileHeader(fs, peContentType);
+
+    case peContentType of
+      e8Bit, eUtf8:
+        psContents := Read8BitFile(fs);
+
+      eUtf16LittleEndian, eUtf16BigEndian:
+        psContents := Read16BitFile(fs, peContentType = eUtf16BigEndian);
+
+      eUtf32LittleEndian, eUtf32BigEndian:
+        psContents := Read32BitFile(fs, peContentType = eUtf32BigEndian);
+
+      else
+        raise Exception.Create('Unknown file content type: ' + IntToStr(Ord(peContentType)));
+
+    end;
+  finally
+    // close the file
+    fs.Free;
+  end;
+end;
+
+
+{
+
     fs.Read(wordRead, SizeOf(wordRead));
     if ((wordRead = Utf16LittleEndianMarker) or (wordRead = Utf16BigEndianMarker)) then
     begin
@@ -146,7 +296,7 @@ begin
         fs.ReadBuffer(contents8bit[1], liBytesRemaining);
       end;
 
-      // convert to wide char 
+      // convert to wide char
       psContents := contents8bit;
     end;
 
@@ -155,6 +305,8 @@ begin
     fs.Free;
   end;
 end;
+
+}
 
 procedure WriteTextFile(const psFileName: string; const psContents: WideString;
   const peContentType: TFileContentType);
