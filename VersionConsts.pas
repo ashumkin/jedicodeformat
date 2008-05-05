84550f0be1c84f90addd578d13385a37c2dbd9c5
AnthonySteele <AnthonySteele@375ba82b-2d0e-0410-9568-ec9673457f6e>
2008-05-05 14:59:21 +0000

    version 2.37
    git-svn-id: https://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2@613 375ba82b-2d0e-0410-9568-ec9673457f6e

--- VersionConsts.pas
+++ VersionConsts.pas
@@ -24,8 +24,8 @@ under the License.
 interface
 
 const
-  PROGRAM_VERSION   = '2.36';
-  PROGRAM_DATE      = 'April 2008';
+  PROGRAM_VERSION   = '2.37';
+  PROGRAM_DATE      = 'May 2008';
   PROGRAM_HOME_PAGE = 'http://jedicodeformat.sourceforge.net/';
   PROGRAM_SOURCEFORGE_HOME_PAGE = 'http://sourceforge.net/projects/jedicodeformat/';
   PROGRAM_SVN_TRUNK = 'http://jedicodeformat.svn.sourceforge.net/svnroot/jedicodeformat/trunk/CodeFormat/Jcf2/';
